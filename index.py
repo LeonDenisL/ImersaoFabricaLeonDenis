f9f0ba029a43877925cced26a721e7f3ac3177a2
Leon Denis Lucena <81835127+LeonDenisL@users.noreply.github.com>
2022-09-12 23:28:07 -0300

    Exercício dia 1
    Adicionando o exercício do primeiro dia do WorkShop da imersão da Fabrica de Soft

---
+++ index.py
@@ -0,0 +1,19 @@
+from modulo_funcao import soma, media, multiplicacao, divisao, subtracao #Chamdando De modulo_a eu quero importar a função soma
+
+
+nome = input('Escreva seu nome no terminal: ')
+
+
+print(nome +' O resultado da SOMA é: ',soma(4,6))
+
+print(nome +' O resultado da SUBTRAÇÃO é: ',subtracao(4,6))
+
+print(nome +' O resultado da DIVISÃO é: ',divisao(4,6))
+
+print(nome +' O resultado da MULTIPLICAÇÃO é: ',multiplicacao(4,6))
+
+print(nome +' O resultado da MÉDIA é: ',media(4,6))
+
+
+
+# Exercicio - Criar dois arquivos python 1 deles com uma função e em outro vai chamar a função2
---
+++ modulo_funcao.py
@@ -0,0 +1,17 @@
+
+def soma(n1,n2):  # def= definir função soma= nome da função, a e b dois valores
+    return (n1+n2)
+
+def subtracao(n1,n2):
+    return (n1-n2)
+
+def multiplicacao(n1,n2):
+    return (n1*n2)
+
+def divisao(n1,n2):
+    return (n1/n2)
+
+def media(n1,n2):
+    return ((n1+n2)/2)
+
+
