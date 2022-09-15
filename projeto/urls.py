69a5fbc2bb1e3181abb99e82b86e3076aa2ed2f4
LeonDenisL <denisleon3@gmail.com>
2022-09-15 17:52:52 -0300

    subindo alterações dia 15/09

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import chiclete, jujuba
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chiclete/', chiclete),
+    path('jujuba/', jujuba),
+
 ]
