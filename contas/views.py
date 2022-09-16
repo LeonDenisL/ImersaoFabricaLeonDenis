d9b6b62ebfbf8954cb659360792dcfa892771fd4
LeonDenisL <denisleon3@gmail.com>
2022-09-16 03:22:01 -0300

    subindo aplicação contas

---
+++ contas/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+import datetime
+from .models import Transacao
+from .form import TransacaoForm
+
+
+# Create your views here.
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['t1','t2','t3']
+    data['now'] = datetime.datetime.now()
+    #html= "<html><body> É essa hora %s.</body></html>" % now
+    return render(request, 'contas/home.html',data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html',data)
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    return render(request,'contas/form.html', data)
